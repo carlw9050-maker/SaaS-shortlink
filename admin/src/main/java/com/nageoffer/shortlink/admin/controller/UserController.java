1249234a2d8267f421c7f9ea2cac91a1634d579b
wangfang <haoxuexi13@163.com>
2025-04-15 13:06:32 +0800

    feature:原始信息的脱敏处理

--- admin/src/main/java/com/nageoffer/shortlink/admin/controller/UserController.java
+++ admin/src/main/java/com/nageoffer/shortlink/admin/controller/UserController.java
@@ -1,7 +1,9 @@
 package com.nageoffer.shortlink.admin.controller;
 
+import cn.hutool.core.bean.BeanUtil;
 import com.nageoffer.shortlink.admin.common.convention.result.Result;
 import com.nageoffer.shortlink.admin.common.convention.result.Results;
+import com.nageoffer.shortlink.admin.dto.resp.UserActualRespDTO;
 import com.nageoffer.shortlink.admin.dto.resp.UserRespDTO;
 import com.nageoffer.shortlink.admin.service.UserService;
 import lombok.RequiredArgsConstructor;
@@ -24,6 +26,13 @@ public class UserController {
     @GetMapping("/api/shortlink/v1/user/{username}")
     public Result<UserRespDTO> getUserByUsername(@PathVariable("username") String username) {
         UserRespDTO result = userService.getUserByUsername(username);
+        return Results.success(result);
+    }
+
+    @GetMapping("/api/shortlink/v1/actual/user/{username}")
+    public Result<UserActualRespDTO> getActualUserByUsername(@PathVariable("username") String username) {
+        UserActualRespDTO result = BeanUtil.toBean(userService.getUserByUsername(username),UserActualRespDTO.class);
+        //将UserRespDTO类型的result对象的属性值复制给UserActualRespDTO类型的result对象的同名属性
         return Results.success(result);
     }
 }
