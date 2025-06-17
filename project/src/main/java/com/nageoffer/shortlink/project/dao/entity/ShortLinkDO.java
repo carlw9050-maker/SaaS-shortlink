089dff31d32f76b28599cb483cf471531e6971c4
wangfang <haoxuexi13@163.com>
2025-06-17 22:58:43 +0800

    feature：mysql中的t_link表中，create_time字段赋不上值的原因找到了：ShortLinkDO里的create_time字段没删除，否则与另一个冲突了。

--- project/src/main/java/com/nageoffer/shortlink/project/dao/entity/ShortLinkDO.java
+++ project/src/main/java/com/nageoffer/shortlink/project/dao/entity/ShortLinkDO.java
@@ -97,11 +97,6 @@ public class ShortLinkDO extends BaseDO {
     @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss",timezone = "GMT+8")
     private Date validDate;
 
-    /**
-     * 创建时间
-     */
-    @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss",timezone = "GMT+8")
-    private Date createTime;
 
     /**
      * 描述
